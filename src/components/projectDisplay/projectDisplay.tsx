01ed49b99c3e0461b8213c23461886a6dc9b268a
John <jashby@student.neumont.edu>
2022-06-14 10:00:18 -0600

    gallery started

--- src/components/projectDisplay/projectDisplay.tsx
+++ src/components/projectDisplay/projectDisplay.tsx
@@ -1,6 +1,16 @@
-import { Typography, Box, Container } from "@mui/material";
+import {
+  Typography,
+  Box,
+  Container,
+  ImageList,
+  ImageListItem,
+} from "@mui/material";
 import { boxStyles } from "./projectDisplayStyles";
 import { RowCenter } from "../../appStyles";
+import Gallery from "react-photo-gallery";
+import React, { useState, useCallback } from "react";
+import { render } from "react-dom";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 const ProjectDisplay = ({ title, desc, img }) => {
   return (
@@ -14,10 +24,99 @@ const ProjectDisplay = ({ title, desc, img }) => {
             WHAT IS {title.toUpperCase()}?
           </Typography>
           <Typography sx={{ mb: 2 }}>{desc}</Typography>
-          <Typography sx={{ mb: 2 }}>IMAGES</Typography>
+          <ImageGallery img={img} />
         </Container>
       </Box>
     </>
+  );
+};
+
+const ImageGallery = ({ img }) => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const [photos, setPhotos] = useState(() => {
+    return [
+      {
+        src: require("../../assets/images/bifrost-logo.jpg"),
+        width: 4,
+        height: 3,
+      },
+      {
+        src: require("../../assets/images/bifrost-logo.jpg"),
+        width: 1,
+        height: 1,
+      },
+      {
+        src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+        width: 4,
+        height: 3,
+      },
+      {
+        src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+        width: 4,
+        height: 3,
+      },
+      {
+        src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+        width: 4,
+        height: 3,
+      },
+    ];
+  });
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+    <div>
+      <Gallery photos={photos} onClick={openLightbox} />
+      {/* @ts-ignore */}
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              // @ts-ignore
+              views={photos.map((x) => ({
+                ...x,
+                // @ts-ignore
+                srcset: x.srcSet,
+                // @ts-ignore
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
   );
 };
 
